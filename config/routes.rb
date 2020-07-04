75a945bf39649253b23194826e5676107af04418
四方 秀 <shu@MacBook-Pro-2.local>
2020-07-04 20:33:24 +0900

    コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show] do 
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
